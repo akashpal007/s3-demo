88ea95f6b7f4df9aa48d1e9c53ea25815cbc344f
Akash Kumar Pal <akashpal@drreddys.com>
2021-08-11 09:42:18 +0530

    Initial commit

---
+++ s3.integration/src/test/java/com/demo/s3/integration/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.s3.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
